63db7c6a5749fa6ab7e8100467e0c67c4e00009b
Shamim Reza <mdshamimreza5552@gmail.com>
2024-11-30 03:40:36 +0600

    rm animation from footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,12 +1,8 @@
-import FadeUpSection from "@/components/FadeUpSection";
-
 const Footer = () => {
   return (
-    <FadeUpSection>
-      <div className="text-slate-300 text-center py-4">
-        Designed by Shamim. All rights reserved. ©
-      </div>
-    </FadeUpSection>
+    <div className="text-slate-300 text-center py-4">
+      Designed by Shamim. All rights reserved. ©
+    </div>
   );
 };
 
